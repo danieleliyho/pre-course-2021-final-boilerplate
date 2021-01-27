240fabcd39cca215ca5beb2dc644d499831f2431
danieleliyho <77384369+danieleliyho@users.noreply.github.com>
2021-01-27 19:59:55 +0200

    add button add date proirty and text inline

--- src/main.js
+++ src/main.js
@@ -2,18 +2,22 @@ let add = document.getElementById('add-button');
 // let todoList = document.getElementsByTagName('ul')
 let todoList = document.getElementById('todo-list');
 let input = document.getElementById("text-input");
+let priority = document.getElementById("priority-selector")
 
 
 add.addEventListener("click",function(){
-    getNewTask ()
+    getNewTask ();
 })
 
 
 function getNewTask(){
     let inputValue = input.value;
+    let priorityValue = priority.value;
     let text = document.createTextNode(inputValue);
+    let priorityValue1 = document.createTextNode(priorityValue);
+    let date = document.createTextNode(new Date());
     if(inputValue === ""){
-        alert("please write some thing")
+        alert("please write some thing");
     }else{
     let divContainer = document.createElement("div");
     let divDate = document.createElement("div");
@@ -27,8 +31,14 @@ function getNewTask(){
     divContainer.appendChild(divDate);
     divContainer.appendChild(divPriority);
     divContainer.appendChild(divText);
-    divText.appendChild(text)}
+    divText.appendChild(text)
+    divPriority.appendChild(priorityValue1)
+    divDate.appendChild(date)
+
+}input.value = "";
 }
+
+
 // function getNewTask () {
 //     let li= document.createElement("li");
 //     let inputValue = input.value;
